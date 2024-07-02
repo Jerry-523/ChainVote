22da2a697761b0f3054548df2d0704a86ef50a73
Péricles Borges <92488227+Jerry-523@users.noreply.github.com>
2024-07-02 20:07:39 +0000

    Create utils.go

---
+++ blockchain_server/blockchain/utils.go
@@ -0,0 +1,19 @@
+package blockchain
+
+import (
+    "bytes"
+    "encoding/json"
+    "net/http"
+)
+
+func PostToServer(url string, data interface{}) (*http.Response, error) {
+    jsonData, err := json.Marshal(data)
+    if err != nil {
+        return nil, err
+    }
+    resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+    if err != nil {
+        return nil, err
+    }
+    return resp, nil
+}
